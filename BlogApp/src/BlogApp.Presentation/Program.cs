35ba8fce671b4ac85b407d0ff821288622bcdc07
Abulqasim <abulqasim.nazarov@gmail.com>
2024-06-30 20:55:21 +0400

    Create mvc

--- BlogApp/src/BlogApp.Presentation/Program.cs
+++ BlogApp/src/BlogApp.Presentation/Program.cs
@@ -1,7 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -9,19 +8,20 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
+
 app.UseRouting();
 
+app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller}/{action=Index}/{id?}");
-
-app.MapFallbackToFile("index.html");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
