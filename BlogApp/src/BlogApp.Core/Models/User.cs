f6d491818d38228a2c62c2116f74e690ccb6fcb5
maksimkins <maks.aleshkov.04@gmail.com>
2024-07-03 00:41:52 +0400

    Create Models

--- BlogApp/src/BlogApp.Core/Models/User.cs
+++ BlogApp/src/BlogApp.Core/Models/User.cs
@@ -1,6 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace BlogApp.Core.Models;
 
 public class User
 {
-    
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string? Name { get; set; }
+    [Required]
+    public string? Surname { get; set; }
+    [Required]
+    public string? Username { get; set; }
+    public string? AvatarUrl { get; set; }
+    [ForeignKey("RoleId"), Required]
+    public int RoleId { get; set; }
+    [Required]
+    public string? Email { get; set; }
 }
