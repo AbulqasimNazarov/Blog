795aed532b32eba69a8f832c8b70974b4c40b8e5
Sadiq <samedov.sadiq55@gmail.com>
2024-07-08 02:23:03 +0400

    Add Login view

--- BlogApp/src/BlogApp.Presentation/Program.cs
+++ BlogApp/src/BlogApp.Presentation/Program.cs
@@ -1,7 +1,35 @@
+using BlogApp.Core.Repositories;
+using BlogApp.Core.Services;
+using BlogApp.Infrastructure.Services;
+using BlogApp.Infrastructure.Repositories.DapperRepositories;
+using BlogApp.Infrastructure.Repositories;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using BlogApp.Presentation.Validators;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var connectionString = builder.Configuration.GetConnectionString("MsSqlServer");
+
+builder.Services.AddSingleton(connectionString);
+
+builder.Services.AddScoped<IRoleRepository, RoleDapperRepository>(provider =>
+    new RoleDapperRepository(connectionString));
+
+builder.Services.AddValidatorsFromAssemblyContaining<UserRegistrationValidator>();
+
+builder.Services.AddControllers().AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<Program>());
+
+builder.Services.AddScoped<IRoleService, RoleService>();
+builder.Services.AddScoped<IUserRoleService, UserRoleService>();
+builder.Services.AddScoped<IUserService, UserService>();
+
+builder.Services.AddScoped<IRoleRepository, RoleDapperRepository>();
+builder.Services.AddScoped<IUserRoleRepository, UserRoleDapperRepository>();
+builder.Services.AddScoped<IUserRepository, UserDapperRepository>();
 
 var app = builder.Build();
 
