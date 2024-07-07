cd4c0fa179f7bfc96e2c6f8fc72ff85a89dc4915
maksimkins <maks.aleshkov.04@gmail.com>
2024-07-07 23:36:26 +0400

    Add Blog

---
+++ BlogApp/src/BlogApp.Core/Models/Blog.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogApp.Core.Models
+{
+    public class Blog
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string? Title { get; set; }
+        [Required]
+        public string? Text { get; set; }
+        [ForeignKey("TopicId"), Required]
+        public int? TopicId { get; set; }
+        [Required]
+        public string? PictureUrl { get; set; } 
+    }
+}
