a785c67f517da28e91888810ab6d5e0086436d9c
maksimkins <maks.aleshkov.04@gmail.com>
2024-07-08 00:06:58 +0400

    Create IBlogService

---
+++ BlogApp/src/BlogApp.Core/Services/IBlogService.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlogApp.Core.Models;
+
+namespace BlogApp.Core.Services
+{
+    public interface IBlogService
+    {
+        public Task CreateBlogAsync(Blog blog);
+        public Task<Blog?> GetBlogByIdAsync(int id);
+        public Task<IEnumerable<Blog?>> GetAllBlogsByUserId(int userId);
+        public Task<IEnumerable<Blog?>> GetAllBlogsByTopic(Topic topic);
+        public Task<IEnumerable<Blog?>> GetAllBlogsByName(string name);
+    }
+}
